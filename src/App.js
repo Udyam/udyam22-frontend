225d36a1f2093a7a7ea2a9614984e375241d73aa
atharva_bhatt <atharvabhatt200@gmail.com>
2021-10-14 00:40:35 +0530

    add navbar and related components, change favicon and  title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header.js";
+import ToggleMenu from "./components/ToggleMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          backgroundImage: "url(background.png)",
+          height: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <div>
+          <Header />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import Reac, { useState } from "react";
+import "./Header.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+import ToggleMenu from "./ToggleMenu";
+
+const Header = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
+
+  return (
+    <>
+      <div>
+        <div>
+          <Navbar
+            dark
+            style={{ backgroundColor: "rgba(202, 240, 248, 0.17)" }}
+            expand="md"
+          >
+            <NavbarBrand href="/">
+              <img src="logo1 4.png" className="logo mx-4" alt="logo" />
+            </NavbarBrand>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="#">
+                  <div className="signin-div px-2">Sign in</div>
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar></UncontrolledDropdown>
+            </Nav>
+            <NavbarToggler className="mx-3" onClick={toggle} />
+            <Collapse isOpen={false} navbar>
+              <Nav className="ms-auto" navbar>
+                <NavItem>
+                  <NavLink href="#">
+                    <div
+                      className="px-4"
+                      style={{
+                        fontWeight: "lighter",
+                        fontSize: "2em",
+                        color: "#CAF0F8",
+                      }}
+                    >
+                      Timeline
+                    </div>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">
+                    <div
+                      className="px-4"
+                      style={{
+                        fontWeight: "lighter",
+                        fontSize: "2em",
+                        color: "#CAF0F8",
+                      }}
+                    >
+                      Contact Us
+                    </div>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">
+                    <div
+                      className="px-4"
+                      style={{
+                        fontSize: "2em",
+                        color: "#CAF0F8",
+                      }}
+                    >
+                      Sign in
+                    </div>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      </div>
+      <div style={{ display: isVisible ? "" : "none" }}>
+        <div style={{ position: "absolute", top: "0" }}>
+          <ToggleMenu toggle={toggle} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
