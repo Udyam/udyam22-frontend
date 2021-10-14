f8abd8e6a1977d7f7adb3acdf3d9c8cf42da09c0
atharva_bhatt <atharvabhatt200@gmail.com>
2021-10-14 11:35:18 +0530

    remove error and rename images and move in proper directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./App.css";
-import Header from "./components/Header.js";
-import ToggleMenu from "./components/ToggleMenu";
+import './App.css'
+import Header from './components/Header.js'
+import ToggleMenu from './components/ToggleMenu'
 
 function App() {
-  return (
-    <div>
-      <div
-        style={{
-          backgroundImage: "url(background.png)",
-          height: "100vh",
-          backgroundSize: "cover",
-        }}
-      >
+    return (
         <div>
-          <Header />
+            <div
+                style={{
+                    backgroundImage: 'url(./images/background.png)',
+                    height: '100vh',
+                    backgroundSize: 'cover',
+                }}
+            >
+                <div>
+                    <Header />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
--- src/components/ToggleMenu.js
+++ src/components/ToggleMenu.js
@@ -1,69 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Toast, ToastBody } from "reactstrap";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import "./ToggleMenu.css";
+import React, { useState, useEffect } from 'react'
+import { Toast, ToastBody } from 'reactstrap'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import './ToggleMenu.css'
 
 const ToggleMenu = ({ toggle }) => {
-  return (
-    <Router>
-      <div style={{ position: "absolute", top: "0" }}>
-        <Toast
-          isOpen={true}
-          style={{
-            height: "100vh",
-            width: "100vw",
-            backgroundColor: "rgba(2, 32, 73, 0.85)",
-          }}
-        >
-          <img
-            src="Group 369.png"
-            alt="close button"
-            className="close-btn mx-4 my-4"
-            onClick={toggle}
-          />
-          <ToastBody>
-            <div className="link-div">
-              <Link to="/" className="toggle-menu-link">
-                HOME
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                ABOUT US
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                EVENTS
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                SPEAKERS
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                LEADERBOARD
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                SPONSORS
-              </Link>
-              <br />
-              <br />
-              <Link to="/" className="toggle-menu-link">
-                TEAM
-              </Link>
-              <br />
-              <br />
+    return (
+        <Router>
+            <div style={{ position: 'absolute', top: '0' }}>
+                <Toast
+                    isOpen={true}
+                    style={{
+                        height: '100vh',
+                        width: '100vw',
+                        backgroundColor: 'rgba(2, 32, 73, 0.85)',
+                    }}
+                >
+                    <img
+                        src="./icon/cross.png"
+                        alt="close button"
+                        className="close-btn mx-4 my-4"
+                        onClick={toggle}
+                    />
+                    <ToastBody>
+                        <div className="link-div">
+                            <Link to="/" className="toggle-menu-link">
+                                HOME
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                ABOUT US
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                EVENTS
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                SPEAKERS
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                LEADERBOARD
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                SPONSORS
+                            </Link>
+                            <br />
+                            <br />
+                            <Link to="/" className="toggle-menu-link">
+                                TEAM
+                            </Link>
+                            <br />
+                            <br />
+                        </div>
+                    </ToastBody>
+                </Toast>
             </div>
-          </ToastBody>
-        </Toast>
-      </div>
-    </Router>
-  );
-};
+        </Router>
+    )
+}
 
-export default ToggleMenu;
+export default ToggleMenu
