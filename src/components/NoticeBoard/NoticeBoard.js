a68d93c349c444bcd043aa029ca5652ffb753ae1
Ayush gangwani <gangwaniah@gmail.com>
2021-11-19 17:07:05 +0530

    dummy commit to change branch

--- src/components/NoticeBoard/NoticeBoard.js
+++ src/components/NoticeBoard/NoticeBoard.js
@@ -3,7 +3,7 @@ import React from "react";
 const Noticeboard = () =>
 {
     return(
-        
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio ea quod voluptatem molestiae maxime sed consequuntur facere mollitia exercitationem voluptates possimus quo, quisquam rem doloremque animi? Obcaecati repellendus nemo eum voluptas facilis, neque ex, soluta, excepturi temporibus atque tempora ullam harum possimus autem nulla nihil dolore laboriosam eveniet. Neque eligendi repellat velit dignissimos inventore assumenda omnis numquam sequi iure cum tempore natus odit veritatis eaque illum, quis ab. Saepe quisquam iusto quas delectus aspernatur ipsam, voluptatum quam expedita quis quibusdam reprehenderit, modi rerum sit ut debitis fugit! Maxime, doloribus! Quia dolore porro numquam qui consectetur iste aperiam commodi magni.</h1>
     );
 }
 
