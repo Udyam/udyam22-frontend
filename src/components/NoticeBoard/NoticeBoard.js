ba8b1f6b73d1921a20499f51fe57df26568d5643
Ayush gangwani <gangwaniah@gmail.com>
2021-11-19 16:42:32 +0530

    Set up routes for NoticeBoard

---
+++ src/components/NoticeBoard/NoticeBoard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Noticeboard = () =>
+{
+    return(
+        
+    );
+}
+
+export default Noticeboard
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Timeline from './components/Timeline/Timeline'
 import { ContactPage } from './components/ContactPage/ContactPage'
 import { Route } from 'react-router-dom'
 import Team from './components/Tabs/Teams/Team.js'
+import NoticeBoard from './components/NoticeBoard/NoticeBoard'
 
 const Routes = () => {
     return (
@@ -33,6 +34,9 @@ const Routes = () => {
                 <Tabs id="panelg">
                     <Team />
                 </Tabs>
+            </Route>
+            <Route path = "/noticeboard">
+                <NoticeBoard />
             </Route>
         </div>
     )
