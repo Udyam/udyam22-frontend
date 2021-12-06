9ab29e9b0d283f39de80ceaa547264ca8e0194ab
PRASHASTI <prashastifuture@gmail.com>
2021-12-06 21:42:35 +0530

    twofilesdlted

--- src/components/authentication/middlewares/no_auth_required.js
+++
@@ -1,9 +0,0 @@
-import { useAuthContext } from '../Context/AuthContext'
-import { useHistory } from 'react-router-dom'
-
-const { token } = useAuthContext()
-const history = useHistory()
-console.log(token)
-if (token) {
-    history.push('/dashboard')
-}
