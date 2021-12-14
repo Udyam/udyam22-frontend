0c048d62b72409196e3ef2400034b80218e0cb15
Yash Garg <63304283+numberbee7070@users.noreply.github.com>
2021-12-14 16:57:50 +0530

    Revert "Tabs branch PR (#29)" (#31)
    This reverts commit c3a2e9a3af4e876d9eab3dfd6b34e44512cb8e2b.

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,13 @@ import Tabs from './components/Tabs/Tabs.js'
 import Timeline from './components/Timeline/Timeline'
 import { ContactPage } from './components/ContactPage/ContactPage'
 import { Route } from 'react-router-dom'
+import RegisterForm from './components/authentication/loginregform'
+import RecoverForm from './components/authentication/recoverpage'
+import ResetForm from './components/authentication/resetpage'
+import NotifyReset from './components/authentication/notifyreset'
 import Team from './components/Tabs/Teams/Team.js'
-import Sponsors from './components/Tabs/Sponsors/Sponsors.js'
-import Event from './components/Tabs/Events/Events.js'
-import Leaderboard from './components/Tabs/Leaderboard/Leaderboard.js'
-import AboutUs from './components/Tabs/AboutUs/AboutUs.js'
-import Speakers from './components/Tabs/Speakers/speakers.js'
+import NoticeBoard from './components/NoticeBoard/NoticeBoard'
+import Dashboard from './components/Dashboard/Dashboard.js'
 
 const Routes = () => {
     return (
@@ -19,33 +20,35 @@ const Routes = () => {
                 <Timeline />
                 <ContactPage />
             </Route>
-            <Route path="/about-us">
-                <Tabs id="panelb">
-                    <AboutUs />
-                </Tabs>
+            <Route exact path="/about-us">
+                <Tabs id="panelb"></Tabs>
             </Route>
-            <Route path="/events">
-                <Tabs id="panelc">
-                    <Event />
-                </Tabs>
+            <Route exact path="/events">
+                <Tabs id="panelc"></Tabs>
             </Route>
             <Route exact path="/speakers">
-                <Tabs id="paneld">{Speakers()}</Tabs>
+                <Tabs id="paneld"></Tabs>
             </Route>
-            <Route path="/leaderboard">
-                <Tabs id="panele">
-                    <Leaderboard />
-                </Tabs>
+            <Route exact path="/leaderboard">
+                <Tabs id="panele"></Tabs>
             </Route>
-            <Route path="/sponsors">
-                <Tabs id="panelf">
-                    <Sponsors />
-                </Tabs>
+            <Route exact path="/sponsors">
+                <Tabs id="panelf"></Tabs>
             </Route>
             <Route path="/team">
                 <Tabs id="panelg">
                     <Team />
                 </Tabs>
+            </Route>
+            <Route exact path="/loginregister" component={RegisterForm} />
+            <Route exact path="/recoverpage" component={RecoverForm} />
+            <Route exact path="/resetpage" component={ResetForm} />
+            <Route exact path="/notifyreset" component={NotifyReset} />
+            <Route path="/noticeboard">
+                <NoticeBoard />
+            </Route>
+            <Route exact path="/dashboard">
+                <Dashboard id="panela" />
             </Route>
         </div>
     )
