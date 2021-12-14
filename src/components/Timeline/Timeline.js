62b79ada280a8b7f556422fec7844a85e1136afe
SanidhyaTaparia <tapariasanidhya@gmail.com>
2021-12-14 19:14:53 +0530

    Ran format

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,82 +5,86 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Timeline = () => {
     return (
         <div className="containertp">
-        <div className="rowtp">
-            <div className="col-md-12">
-            <div className="d-flex flex-row-reverse">
-                <img
-                    src="./images/timelineImage.png"
-                    alt="Timeline"
-                    className="p-2 img-fluid imageTimeline"
-                />
-            </div>
+            <div className="rowtp">
+                <div className="col-md-12">
+                    <div className="d-flex flex-row-reverse">
+                        <img
+                            src="./images/timelineImage.png"
+                            alt="Timeline"
+                            className="p-2 img-fluid imageTimeline"
+                        />
+                    </div>
 
-                <div className="main-timeline3">
-                    <div className="timeline">
-                         <div className="timeline-content">
-                            <h2>DD MM YY</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing
-                                elit. Distinctio molestiae sequi eius doloribus facilis,
-                                sed omnis, a adipisci necessitatibus quod voluptas nemo
-                                architecto maxime accusamus, perferendis vero? Sequi,
-                                maiores unde?
-                            </p>
-                        </div>
-                    </div>
-                    <div className="timeline">
-                        <div className="timeline-content">
-                            <h2>DD MM YY</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing
-                                elit. Distinctio molestiae sequi eius doloribus facilis,
-                                sed omnis, a adipisci necessitatibus quod voluptas nemo
-                                architecto maxime accusamus, perferendis vero? Sequi,
-                                maiores unde?
-                            </p>
+                    <div className="main-timeline3">
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h2>DD MM YY</h2>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Distinctio molestiae sequi
+                                    eius doloribus facilis, sed omnis, a
+                                    adipisci necessitatibus quod voluptas nemo
+                                    architecto maxime accusamus, perferendis
+                                    vero? Sequi, maiores unde?
+                                </p>
                             </div>
-                    </div>
-                    <div className="timeline">
-                        <div className="timeline-content">
-                            <h2>DD MM YY</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing
-                                elit. Distinctio molestiae sequi eius doloribus facilis,
-                                sed omnis, a adipisci necessitatibus quod voluptas nemo
-                                architecto maxime accusamus, perferendis vero? Sequi,
-                                maiores unde?
-                            </p>
-                            </div>
-                    </div>
-                    <div className="timeline">
-                        <div className="timeline-content">
-                            <h2>DD MM YY</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing
-                                elit. Distinctio molestiae sequi eius doloribus facilis,
-                                sed omnis, a adipisci necessitatibus quod voluptas nemo
-                                architecto maxime accusamus, perferendis vero? Sequi,
-                                maiores unde?
-                            </p>
                         </div>
-                    </div>
-                    <div className="timeline">
-                        <div className="timeline-content">
-                            <h2>DD MM YY</h2>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing
-                                elit. Distinctio molestiae sequi eius doloribus facilis,
-                                sed omnis, a adipisci necessitatibus quod voluptas nemo
-                                architecto maxime accusamus, perferendis vero? Sequi,
-                                maiores unde?
-                            </p>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h2>DD MM YY</h2>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Distinctio molestiae sequi
+                                    eius doloribus facilis, sed omnis, a
+                                    adipisci necessitatibus quod voluptas nemo
+                                    architecto maxime accusamus, perferendis
+                                    vero? Sequi, maiores unde?
+                                </p>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h2>DD MM YY</h2>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Distinctio molestiae sequi
+                                    eius doloribus facilis, sed omnis, a
+                                    adipisci necessitatibus quod voluptas nemo
+                                    architecto maxime accusamus, perferendis
+                                    vero? Sequi, maiores unde?
+                                </p>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h2>DD MM YY</h2>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Distinctio molestiae sequi
+                                    eius doloribus facilis, sed omnis, a
+                                    adipisci necessitatibus quod voluptas nemo
+                                    architecto maxime accusamus, perferendis
+                                    vero? Sequi, maiores unde?
+                                </p>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h2>DD MM YY</h2>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Distinctio molestiae sequi
+                                    eius doloribus facilis, sed omnis, a
+                                    adipisci necessitatibus quod voluptas nemo
+                                    architecto maxime accusamus, perferendis
+                                    vero? Sequi, maiores unde?
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
     )
 }
 
