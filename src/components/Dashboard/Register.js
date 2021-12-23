ef6c5acc0ece56491b3f2210ddc94be82644ffa8
Prashasti Tripathi <90844054+tripathiprash@users.noreply.github.com>
2021-12-23 18:42:57 +0530

    Tokencondition (#42)
    * logout function is added to dashboard
    
    * doen
    
    * Added all dashboard apis
    
    * Ran Format and Lint
    
    * commit
    
    * Added redirection in /loginregister and /comingsoon
    
    * token condition added finally
    
    * format
    
    * comments removed
    
    Co-authored-by: SanidhyaTaparia <tapariasanidhya@gmail.com>
    Co-authored-by: Ayush gangwani <gangwaniah@gmail.com>

--- src/components/Dashboard/Register.js
+++ src/components/Dashboard/Register.js
@@ -1,23 +1,95 @@
 import React from 'react'
-import { Form, FormGroup, Input } from 'reactstrap'
-// import isEmail from 'validator/lib/isEmail'
-// import isPhone from 'validator/lib/isMobilePhone'
+import { Form, FormGroup, FormFeedback, Input } from 'reactstrap'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import isEmail from 'validator/lib/isEmail'
 import './Register.css'
 // import axios from 'axios'
 
 //api
-//https://udyam22-backend.herokuapp.com/auth/update/
+
+const API_BASE_URL = 'https://udyam22-backend.herokuapp.com'
 
 const Register = () => {
-    // const [input, setInput] = useState({
-    //     email: '',
-    //     phone: '',
-    // })
+    const [input, setInput] = useState({
+        teamname: '',
+        event: '',
+        leader: '',
+        member1: '',
+        member2: '',
+    })
+    const [numMembers, setNumMembers] = useState('Members')
+    const [check, setCheck] = useState(0)
 
-    // const inputChangeHandler = (e) => {
-    //     const newInput = { ...input }
-    //     newInput[e.target.name] = e.target.value
-    //     setInput(newInput)
+    const [firstmember, setFirstmember] = useState(false)
+    const [secondmember, setSecondmember] = useState(false)
+    const [thirdmember, setThirdmember] = useState(false)
+
+    const inputChangeHandler = async (e) => {
+        // console.log("e in form", e.target.value);
+        const newInput = { ...input }
+        newInput[e.target.name] = e.target.value
+        setInput(newInput)
+        // console.log("event=",input.event);
+    }
+
+    const memberChangeHandler = async (e) => {
+        // console.log("e in form", e.target.value);
+        setNumMembers(e.target.value)
+        showDropdown(e.target.value)
+        // console.log("numMembers=",numMembers);
+    }
+
+    const showDropdown = (num) => {
+        if (num === '1') {
+            setFirstmember(true)
+            setSecondmember(false)
+            setThirdmember(false)
+        } else if (num === '2') {
+            setFirstmember(true)
+            setSecondmember(true)
+            setThirdmember(false)
+        } else if (num === '3') {
+            setFirstmember(true)
+            setSecondmember(true)
+            setThirdmember(true)
+        }
+    }
+
+    // function submitHandler(e) {
+    //     console.log("e",e);
+    useEffect(() => {
+        if (check) {
+            console.log('check=', check)
+            console.log('input=', input)
+
+            // if number of members is 1 dlete member1 and member2 from input
+            if (numMembers === '1') {
+                delete input.member1
+                delete input.member2
+            }
+            // if number of members is 2 delete member2 from input
+            else if (numMembers === '2') {
+                delete input.member2
+            }
+
+            console.log('input new=', input)
+
+            axios
+                .post(API_BASE_URL + '/API/team/create/', input, {
+                    headers: {
+                        Authorization:
+                            'Token d102f9b8531448411f3658ecfdeeee5b0fbf2a17',
+                    },
+                })
+                .then((res) => {
+                    console.log(res)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }, [check])
     // }
 
     return (
@@ -62,17 +134,26 @@ const Register = () => {
 
                     <FormGroup>
                         {/* <Input type="select" name="select" id="exampleSelect" placeholder="EVENT"> */}
-                        <select name="EVENT" id="id_event" className="dropdown">
-                            <option>EVENT</option>
-                            <option>MOSAIC</option>
-                            <option>SPYBITS</option>
-                            <option>I-CHIP</option>
-                            <option>COMMNET</option>
-                            <option>CONTINNUM</option>
-                            <option>DIGISM</option>
-                            <option>XIOTA</option>
-                            <option>CASSANDRA</option>
-                            <option>FUNCKIT</option>
+                        <select
+                            name="event"
+                            value={input.event}
+                            onChange={(e) => {
+                                inputChangeHandler(e)
+                                console.log('event e=', input.event)
+                            }}
+                            id="id_event"
+                            className="dropdown"
+                        >
+                            <option value="EVENT">EVENT</option>
+                            <option value="MOSAIC">MOSAIC</option>
+                            <option value="SPYBITS">SPYBITS</option>
+                            <option value="I-CHIP">I-CHIP</option>
+                            <option value="COMMNET">COMMNET</option>
+                            <option value="CONTINNUM">CONTINNUM</option>
+                            <option value="DIGISM">DIGISM</option>
+                            <option value="XIOTA">XIOTA</option>
+                            <option value="CASSANDRA">CASSANDRA</option>
+                            <option value="FUNCKIT">FUNCKIT</option>
                         </select>
                         {/* </Input> */}
                     </FormGroup>
@@ -80,8 +161,12 @@ const Register = () => {
                     <FormGroup>
                         <Input
                             type="text"
-                            name="name"
+                            name="teamname"
                             placeholder="TEAM NAME"
+                            value={input.teamname}
+                            onChange={(e) => {
+                                inputChangeHandler(e)
+                            }}
                             className="team"
                             required
                         />
@@ -98,28 +183,133 @@ const Register = () => {
                     <FormGroup>
                         {/* <Input type="select" name="select" id="exampleSelect" placeholder="EVENT"> */}
                         <select
-                            name="MEMBERS"
+                            name="members"
                             id="id_event"
                             className="dropdown"
+                            value={numMembers}
+                            onChange={(e) => {
+                                memberChangeHandler(e)
+                            }}
                         >
-                            <option>MEMBERS</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
+                            <option value="Members">MEMBERS</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
                         </select>
                         {/* </Input> */}
                     </FormGroup>
-                    {/* <div id="contact-form-button-wrapper">
-                                <button type="submit" id="contact-form-button">
-                                    {' '}
-                                    Submit{' '}
-                                </button>
-                            </div> */}
+
+                    {firstmember && (
+                        <FormGroup>
+                            <Input
+                                type="email"
+                                name="leader"
+                                value={input.leader}
+                                valid={
+                                    input.leader !== '' && isEmail(input.leader)
+                                }
+                                invalid={
+                                    input.leader !== '' &&
+                                    !isEmail(input.leader)
+                                }
+                                onChange={(e) => {
+                                    inputChangeHandler(e)
+                                }}
+                                placeholder="TEAM LEADER"
+                                className="team"
+                                required
+                            />
+
+                            <FormFeedback className="contact-form-feedback">
+                                {' '}
+                                Please enter a valid Leader email{' '}
+                            </FormFeedback>
+                            <FormFeedback
+                                valid
+                                style={{ display: 'none' }}
+                                className="contact-form-feedback"
+                            ></FormFeedback>
+                        </FormGroup>
+                    )}
+
+                    {secondmember && (
+                        <FormGroup>
+                            <Input
+                                type="email"
+                                name="member1"
+                                value={input.member1}
+                                valid={
+                                    input.member1 !== '' &&
+                                    isEmail(input.member1)
+                                }
+                                invalid={
+                                    input.member1 !== '' &&
+                                    !isEmail(input.member1)
+                                }
+                                onChange={(e) => {
+                                    inputChangeHandler(e)
+                                }}
+                                placeholder="MEMBER 1"
+                                className="team"
+                            />
+
+                            <FormFeedback className="contact-form-feedback">
+                                {' '}
+                                Please enter a valid Member 1 email{' '}
+                            </FormFeedback>
+                            <FormFeedback
+                                valid
+                                style={{ display: 'none' }}
+                                className="contact-form-feedback"
+                            ></FormFeedback>
+                        </FormGroup>
+                    )}
+
+                    {thirdmember && (
+                        <FormGroup>
+                            <Input
+                                type="email"
+                                name="member2"
+                                value={input.member2}
+                                valid={
+                                    input.member2 !== '' &&
+                                    isEmail(input.member2)
+                                }
+                                invalid={
+                                    input.member2 !== '' &&
+                                    !isEmail(input.member2)
+                                }
+                                onChange={(e) => {
+                                    inputChangeHandler(e)
+                                }}
+                                placeholder="MEMBER 2"
+                                className="team"
+                            />
+
+                            <FormFeedback className="contact-form-feedback">
+                                {' '}
+                                Please enter a valid Member 2 email{' '}
+                            </FormFeedback>
+                            <FormFeedback
+                                valid
+                                style={{ display: 'none' }}
+                                className="contact-form-feedback"
+                            ></FormFeedback>
+                        </FormGroup>
+                    )}
                 </Form>
             </div>
 
             <div className="buttonContainer">
-                <button className="button">Submit</button>
+                <button
+                    className="button"
+                    onClick={() => {
+                        console.log('button clicked')
+                        setCheck(check + 1)
+                    }}
+                >
+                    Submit
+                </button>
             </div>
 
             {/* </div> */}
--- src/components/authentication/loginregform.js
+++ src/components/authentication/loginregform.js
@@ -10,11 +10,12 @@ import { Link, useHistory } from 'react-router-dom'
 toast.configure()
 
 export default function RegisterForm() {
+    const history = useHistory()
+    if (localStorage.getItem('userToken')) history.push('/comingsoon')
+
     const [user_email, setuser_email] = useState('')
     const [user_pass, setuser_pass] = useState('')
-    /* const {setToken , setData} = useAuthContext();*/
     const { setToken } = useAuthContext()
-    const history = useHistory()
 
     const userlogin = (e) => {
         e.preventDefault()
@@ -37,9 +38,6 @@ export default function RegisterForm() {
                     position: toast.POSITION.BOTTOM_RIGHT,
                 })
                 {
-                    console.log(response.data.token)
-                    /* setData(response.data.data);
-              setToken(response.data.token);*/
                     setToken(response.data.token)
                     history.push('/comingsoon')
                 }
@@ -59,7 +57,6 @@ export default function RegisterForm() {
     const [referalcode, setReferalcode] = useState('')
     const [confirmpassword, setConfirmpassword] = useState('')
     const [year, setYear] = useState('')
-    /*const [image, setImage] = useState('../images/Group2471.png')*/
 
     const registerFieldsAreValid = (
         Name,
@@ -107,11 +104,6 @@ export default function RegisterForm() {
         }
         return true
     }
-    /* const onImageChange = (event) => {
-        if (event.target.files && event.target.files[0]) {
-            setImage(URL.createObjectURL(event.target.files[0]))
-        }
-    }*/
 
     const register = (e) => {
         e.preventDefault()
@@ -154,8 +146,6 @@ export default function RegisterForm() {
                     )
                     {
                         console.log(response.data.token)
-                        /*setData(response.data.data);
-            setToken(response.data.token);*/
                         setToken(response.data.token)
                         history.push('/comingsoon')
                     }
@@ -245,14 +235,6 @@ export default function RegisterForm() {
                         <div className="loginregform-overlay">
                             <div className="loginregform-overlay-panel loginregform-overlay-left">
                                 <form className="loginregform-form">
-                                    {/* <div className="profile-pic-div">
-                                        <img src={image} id="photo" />
-                                        <input
-                                            type="file"
-                                            id="dp-file"
-                                            onChange={onImageChange}
-                                        />
-                            </div>*/}{' '}
                                     <h1 className="loginregform-h12">
                                         SIGN UP
                                     </h1>
