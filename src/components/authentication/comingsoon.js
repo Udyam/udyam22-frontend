c2672a669009706d4a5b3cc8780e20c02d731171
PRASHASTI <prashastifuture@gmail.com>
2021-12-26 18:21:43 +0530

    logout function added with correct design

---
+++ src/components/authentication/comingsoon.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './comingsoon.css'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+import ComingSoon from './registration.js'
+import { Link, useHistory } from 'react-router-dom'
+import { useAuthContext } from '../authentication/Context/AuthContext'
+
+const logoutcoming = () => {
+    const history = useHistory()
+    const { logout } = useAuthContext()
+    const logoutfn = () => {
+        logout()
+        history.push('/')
+    }
+    return (
+        <div>
+            <div className="dashboardContainer">
+                <div className="displayPictureDash">
+                    <img className="displayPicture" src="./icon/dummy.png" />
+                    <Link to="#">
+                        <img className="editIconDash" src="./icon/edit.png" />
+                    </Link>
+                </div>
+                <div className="imageContainer">
+                    <img
+                        src="./images/udyamLogo.png"
+                        className="displayImage"
+                    />
+                </div>
+                <div className="tabs">
+                    <Nav className="justify-content-center">
+                        <div className="navigation">
+                            <NavItem>
+                                <NavLink className="tabLinks" href="#profile">
+                                    ComingSoon
+                                </NavLink>
+                            </NavItem>
+                            
+                            <NavItem>
+                                <NavLink className="tabLinks" href="#">
+                                    Logout
+                                </NavLink>
+                            </NavItem>
+                        </div>
+                        <div className="navigationMobile">
+                            <img
+                                className="navLogo"
+                                src="./images/udyamLogo.png"
+                            />
+                            <NavItem>
+                                <NavLink
+                                    className="tabLinks"
+                                    href="#"
+                                    onClick={logoutfn}
+                                >
+                                    <img
+                                        src="./icon/log-out.png"
+                                        className="logoutIcon"
+                                    />
+                                    logout
+                                </NavLink>
+                            </NavItem>
+                        </div>
+                    </Nav>
+                </div>
+                <div className="scrollContainer px-4">
+                    <ul style={{ listStyleType: 'none' }}>
+                        <li>
+                            <h2 id="profile">The event registration will be starting soon</h2>
+                            <ComingSoon />
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default logoutcoming
--- src/components/authentication/registration.js
+++ src/components/authentication/registration.js
@@ -1,56 +1,21 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 import './registration.css'
-import { Nav, NavItem, NavLink } from 'reactstrap'
-import { useAuthContext } from './Context/AuthContext'
 
-export default function ComingSoon() {
-    const history = useHistory()
-    if (
-        !localStorage.getItem('userToken') ||
-        localStorage.getItem('userToken') == 'undefined'
-    )
-        history.push('/loginregister')
 
-    const { logout } = useAuthContext()
-    const logoutfn = () => {
-        logout()
-        history.push('/')
-    }
+const ComingSoon = () => {
 
     return (
-        <div className="comingsoonwholepage">
-            <div className="comingsoon-logocircle12">
-                <img
-                    src="../images/photo_2021-11-27_18-13-10.png"
-                    className="comingsoon-Udyamlogo12"
-                    alt="Udyam"
-                />
-            </div>
-            <div className="comingsoon-form-container">
-                <div className="tabs coming">
-                    <Nav className="justify-content-center">
-                        <div className="navigation">
-                            <NavItem>
-                                <NavLink className="tabLinks" href="#">
-                                    Logout
-                                </NavLink>
-                            </NavItem>
-                        </div>
-                        <div className="navigationMobile">
-                            <NavItem>
-                                <NavLink onClick={logoutfn}>logout</NavLink>
-                            </NavItem>
-                        </div>
-                    </Nav>
-                </div>
-
-                <div className="comingsoon-noti">
-                    <h1 className="comingsoon-form-head">
-                        Event Registration will open soon!!
-                    </h1>
-                </div>
-            </div>
+        <div className="profileContainer">
+            <ul style={{ listStyleType: 'none' }}>
+                <li>
+                    <h4 style={{ fontWeight: '600', marginTop: '20px' }}>
+                        Till then please wait.
+                    </h4>
+                </li>
+               
+            </ul>
         </div>
     )
 }
+
+export default ComingSoon
