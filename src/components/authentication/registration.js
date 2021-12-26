80cd381209a425cc6dcc1f5fa7a2a9fb6478be00
PRASHASTI <prashastifuture@gmail.com>
2021-12-26 17:45:16 +0530

    logout added to coming soon

--- src/components/authentication/registration.js
+++ src/components/authentication/registration.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import './registration.css'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+import { useAuthContext } from './Context/AuthContext'
 
 export default function ComingSoon() {
     const history = useHistory()
@@ -9,6 +11,12 @@ export default function ComingSoon() {
         localStorage.getItem('userToken') == 'undefined'
     )
         history.push('/loginregister')
+
+        const { logout } = useAuthContext()
+        const logoutfn = () => {
+            logout()
+            history.push('/')
+        }
 
     return (
         <div className="comingsoonwholepage">
@@ -20,6 +28,29 @@ export default function ComingSoon() {
                 />
             </div>
             <div className="comingsoon-form-container">
+            <div className="tabs coming">
+                    <Nav className="justify-content-center">
+                        <div className="navigation">
+                    
+                            <NavItem>
+                                <NavLink className="tabLinks" href="#">
+                                    Logout
+                                </NavLink>
+                            </NavItem>
+                        </div>
+                        <div className="navigationMobile">
+                            <NavItem>
+                                <NavLink
+                                  
+                                    onClick={logoutfn}
+                                >
+                                    logout
+                                </NavLink>
+                            </NavItem>
+                        </div>
+                    </Nav>
+                </div>
+          
                 <div className="comingsoon-noti">
                     <h1 className="comingsoon-form-head">
                         Event Registration will open soon!!
