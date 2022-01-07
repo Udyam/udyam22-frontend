7e5466a5df3012bcb8480b49f867311476b5fe0f
Sarang Manerikar <sarang.manerikar.ece20@itbhu.ac.in>
2022-01-07 18:24:24 +0530

    Recently SUggested Changes Added
    Co-authored-by: Jayesh Upadhyay
    Co-authored-by: Mayank Singh

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -4,89 +4,72 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Timeline = () => {
     return (
-        <div id="timeline-main-container" className="containertp">
-            <div className="rowtp">
-                <div className="col-md-12">
-                    <div className="d-flex flex-row-reverse">
-                        {/* <img
-                            src="./images/timelineImage.png"
-                            alt="Timeline"
-                            className="p-2 img-fluid imageTimeline"
-                        /> */}
-
-                        <h1 className="Timeline_head">TIMELINE</h1>
-                    </div>
-
-                    <div className="main-timeline3">
-                        <div className="timeline">
-                            <div className="timeline-content">
-                                <h2>DD MM YY</h2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Distinctio molestiae sequi
-                                    eius doloribus facilis, sed omnis, a
-                                    adipisci necessitatibus quod voluptas nemo
-                                    architecto maxime accusamus, perferendis
-                                    vero? Sequi, maiores unde?
-                                </p>
-                            </div>
-                        </div>
-                        <div className="timeline">
-                            <div className="timeline-content">
-                                <h2>DD MM YY</h2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Distinctio molestiae sequi
-                                    eius doloribus facilis, sed omnis, a
-                                    adipisci necessitatibus quod voluptas nemo
-                                    architecto maxime accusamus, perferendis
-                                    vero? Sequi, maiores unde?
-                                </p>
-                            </div>
-                        </div>
-                        <div className="timeline">
-                            <div className="timeline-content">
-                                <h2>DD MM YY</h2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Distinctio molestiae sequi
-                                    eius doloribus facilis, sed omnis, a
-                                    adipisci necessitatibus quod voluptas nemo
-                                    architecto maxime accusamus, perferendis
-                                    vero? Sequi, maiores unde?
-                                </p>
-                            </div>
-                        </div>
-                        <div className="timeline">
-                            <div className="timeline-content">
-                                <h2>DD MM YY</h2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Distinctio molestiae sequi
-                                    eius doloribus facilis, sed omnis, a
-                                    adipisci necessitatibus quod voluptas nemo
-                                    architecto maxime accusamus, perferendis
-                                    vero? Sequi, maiores unde?
-                                </p>
-                            </div>
-                        </div>
-                        <div className="timeline">
-                            <div className="timeline-content">
-                                <h2>DD MM YY</h2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Distinctio molestiae sequi
-                                    eius doloribus facilis, sed omnis, a
-                                    adipisci necessitatibus quod voluptas nemo
-                                    architecto maxime accusamus, perferendis
-                                    vero? Sequi, maiores unde?
-                                </p>
-                            </div>
-                        </div>
+        <>
+            <div className="d-flex flex-row-reverse">
+                <img
+                    src="./images/timelineImage.png"
+                    alt="Timeline"
+                    className="p-2 img-fluid imageTimeline"
+                />
+            </div>
+            <div className="timeline" id="timeline-main-container">
+                <div className="container-time left">
+                    <div className="content-time">
+                        <h2>
+                            <time>5 DEC 2021</time>
+                        </h2>
+                        <p>
+                            Phase 1 of our workshops. Join us as we start from
+                            the basics of each event. Uploaded to YouTube every
+                            Monday, Wednesday and Friday.
+                        </p>
                     </div>
                 </div>
+                <div className="container-time right">
+                    <div className="content-time">
+                        <h2>1 JAN 2022</h2>
+                        <p>
+                            Phase 2 of our workshops. The continuation of the
+                            workshop series, covering all the important
+                            theories, technologies and tricks. Uploaded to
+                            YouTube every Saturday and Sunday.
+                        </p>
+                    </div>
+                </div>
+                <div className="container-time left">
+                    <div className="content-time">
+                        <h2>1 FEB 2022</h2>
+                        <p>
+                            Phase 3 of our Workshops. Discussing the all
+                            important problem statements and winding up on all
+                            the learning. Uploaded to YouTube every Saturday and
+                            Sunday.
+                        </p>
+                    </div>
+                </div>
+                <div className="container-time right">
+                    <div className="content-time">
+                        <h2>8 APRIL 2022</h2>
+                        <p>
+                            Buckle up for UDYAM&apos;22!! Eventwise schedule
+                            coming soon.
+                        </p>
+                    </div>
+                </div>
+                {/* <div className="container-time left">
+                    <div className="content-time">
+                        <h2>DD MM YY</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Distinctio molestiae sequi eius doloribus
+                            facilis, sed omnis, a adipisci necessitatibus quod
+                            voluptas nemo architecto maxime accusamus,
+                            perferendis vero? Sequi, maiores unde?
+                        </p>
+                    </div>
+                </div> */}
             </div>
-        </div>
+        </>
     )
 }
 
