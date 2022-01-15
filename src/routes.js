5dcabd6f77e51b9c64f6f70def5fd441a75cbd8f
Jayesh Upadhyay <78408997+jayeshupadhyay@users.noreply.github.com>
2022-01-15 16:46:43 +0530

    Recent Issues Resolved (#88)
    Co-authored-by: Sarang Manerikar <sarang.manerikar.ece20@itbhu.ac.in>
    
    Co-authored-by: Sarang Manerikar <sarang.manerikar.ece20@itbhu.ac.in>

--- src/routes.js
+++ src/routes.js
@@ -19,50 +19,41 @@ import Dashboard from './components/Dashboard/Dashboard.js'
 
 const Routes = () => {
     return (
-        <>
+        <div>
             <Route exact path="/">
                 <LandingPage />
                 <Timeline />
                 <ContactPage />
             </Route>
-            <div
-                style={{
-                    backgroundImage: 'url(/images/background.png)',
-                    height: 'auto',
-                    backgroundSize: 'cover',
-                    backgroundAttachment: 'fixed',
-                }}
-            >
-                <Route path="/about-us">
-                    <Tabs id="panelb">
-                        <AboutUs />
-                    </Tabs>
-                </Route>
-                <Route path="/events">
-                    <Tabs id="panelc">
-                        <Event />
-                    </Tabs>
-                </Route>
-                <Route path="/speakers">
-                    <Tabs id="paneld">{Speakers()}</Tabs>
-                    {/* {Speakers()} */}
-                </Route>
-                <Route path="/leaderboard">
-                    <Tabs id="panele">
-                        <Leaderboard />
-                    </Tabs>
-                </Route>
-                <Route path="/sponsors">
-                    <Tabs id="panelf">
-                        <Sponsors />
-                    </Tabs>
-                </Route>
-                <Route path="/team">
-                    <Tabs id="panelg">
-                        <Team />
-                    </Tabs>
-                </Route>
-            </div>
+            <Route path="/about-us">
+                <Tabs id="panelb">
+                    <AboutUs />
+                </Tabs>
+            </Route>
+            <Route path="/events">
+                <Tabs id="panelc">
+                    <Event />
+                </Tabs>
+            </Route>
+            <Route path="/speakers">
+                <Tabs id="paneld">{Speakers()}</Tabs>
+                {/* {Speakers()} */}
+            </Route>
+            <Route path="/leaderboard">
+                <Tabs id="panele">
+                    <Leaderboard />
+                </Tabs>
+            </Route>
+            <Route path="/sponsors">
+                <Tabs id="panelf">
+                    <Sponsors />
+                </Tabs>
+            </Route>
+            <Route path="/team">
+                <Tabs id="panelg">
+                    <Team />
+                </Tabs>
+            </Route>
             <Route exact path="/loginregister" component={RegisterForm} />
             <Route exact path="/recoverpage" component={RecoverForm} />
             <Route exact path="/resetpage" component={ResetForm} />
@@ -76,7 +67,7 @@ const Routes = () => {
             <Route exact path="/dashboard">
                 <Dashboard id="panela" />
             </Route>
-        </>
+        </div>
     )
 }
 
