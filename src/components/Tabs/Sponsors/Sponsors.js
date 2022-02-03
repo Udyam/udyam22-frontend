9cb314eb782b98002c5794358b1aedf41ddeb1a9
Sarang Manerikar <sarang.manerikar.ece20@itbhu.ac.in>
2022-02-03 22:35:42 +0530

    Added Latest Sponsors
    Co-authored-by: Mayank Singh <mayank.singh.ece20@itbhu.ac.in>
    Co-authored-by: Jayesh Upadhyay <jayesh.upadhyay.ece20@itbhu.ac.in>

--- src/components/Tabs/Sponsors/Sponsors.js
+++ src/components/Tabs/Sponsors/Sponsors.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Sponsors.css'
-import nvidia from './Sponsors_Images_Folder/NVIDIA.jpeg'
-import arm from './Sponsors_Images_Folder/ARM.jpeg'
-import cisco from './Sponsors_Images_Folder/CISCO.jpeg'
-import mentor_graphics from './Sponsors_Images_Folder/MENTOR_GRAPHICS.jpeg'
+import Coin_Kuber from './Sponsors_Images_Folder/Coin_Kuber.jpg'
+import Aptron from './Sponsors_Images_Folder/Aptron.png'
+import Cisco from './Sponsors_Images_Folder/Cisco.png'
+import Silence_Labs from './Sponsors_Images_Folder/Silence_Labs.png'
+import Mentor from './Sponsors_Images_Folder/Mentor.jpg'
+import Intec_Info from './Sponsors_Images_Folder/Intec_Info.jfif'
+import Teeshood from './Sponsors_Images_Folder/Teeshood.png'
+import Cisco_Thing from './Sponsors_Images_Folder/Cisco_Thing.png'
+import CodeChef from './Sponsors_Images_Folder/CodeChef.jpg'
+import Persistent from './Sponsors_Images_Folder/Persistent.jfif'
+import Nvidia from './Sponsors_Images_Folder/Nvidia.png'
+import silverpush from './Sponsors_Images_Folder/silverpush.png'
+import Appwars from './Sponsors_Images_Folder/Appwars.jfif'
+import Rapid_Ai from './Sponsors_Images_Folder/Rapid_Ai.jpg'
 
 const Sponsors = () => {
     return (
@@ -46,29 +56,136 @@ const Sponsors = () => {
                 </div> */}
                 <div className="row-2">
                     <div className="subheading">
-                        <h4>PREVIOUS SPONSORS :</h4>
-                    </div>
-                    <div className="title2">
-                        <div className="box">
-                            <img src={cisco} className="sponsors_image_box" />
-                        </div>
+                        <h4>PREVIOUS TITLE SPONSORS :</h4>
                     </div>
                     <div className="title2">
                         <div className="box">
                             <img
-                                src={mentor_graphics}
+                                src={Coin_Kuber}
                                 className="sponsors_image_box"
                             />
                         </div>
                     </div>
                     <div className="title2">
                         <div className="box">
-                            <img src={nvidia} className="sponsors_image_box" />
+                            <img src={Aptron} className="sponsors_image_box" />
                         </div>
                     </div>
                     <div className="title2">
                         <div className="box">
-                            <img src={arm} className="sponsors_image_box" />
+                            <img src={Cisco} className="sponsors_image_box" />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Silence_Labs}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img src={Mentor} className="sponsors_image_box" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row-2">
+                    <div className="subheading">
+                        <h4>PREVIOUS CO-TITLE SPONSORS :</h4>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Intec_Info}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Teeshood}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Cisco_Thing}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img src={Aptron} className="sponsors_image_box" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row-2">
+                    <div className="subheading">
+                        <h4>PREVIOUS EVENT SPONSORS :</h4>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={CodeChef}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img src={Nvidia} className="sponsors_image_box" />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={silverpush}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Silence_Labs}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Persistent}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img src={Appwars} className="sponsors_image_box" />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Cisco_Thing}
+                                className="sponsors_image_box"
+                            />
+                        </div>
+                    </div>
+                    <div className="title2">
+                        <div className="box">
+                            <img
+                                src={Rapid_Ai}
+                                className="sponsors_image_box"
+                            />
                         </div>
                     </div>
                 </div>
