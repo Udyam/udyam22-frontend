e50b8b81b3f3b49cc5a0d8ea9c7c41bfc91e47f8
Jayesh Upadhyay <jayesh.upadhyay.ece20@itbhu.ac.in>
2022-02-16 00:31:34 +0530

    Changes as per latest suggestions
    Co-authored-by: Sarang Manerikar <sarang.manerikar.ece20@itbhu.ac.in>
    Co-authored-by: Mayank Singh <mayank.singh.ece20@itbhu.ac.in>

--- src/components/Tabs/Teams/Designers.js
+++ src/components/Tabs/Teams/Designers.js
@@ -1,19 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Sub_Files.css'
-import Carousel from 'react-simply-carousel'
 import { Route, Link } from 'react-router-dom'
 import * as Members from './Members_Description_Tiles.js'
-import Forward from './Icons/Front_Arrow.png'
-import Backward from './Icons/Back_Arrow.png'
 
 const Designers = () => {
-    const [activeSlide, setActiveSlide] = useState(0)
-
-    function CheckBrowser() {
-        if (navigator.userAgent.indexOf('Firefox') > -1) return 0
-        return 500
-    }
-
     return (
         <Route>
             <div className="container_box" id="Teams_Scrollbar">
@@ -22,89 +12,39 @@ const Designers = () => {
                 </Link>
                 <h1 className="title">DESIGNERS</h1>
                 <div className="Teams_Carousel_Container">
-                    <Carousel
-                        disableNavIfAllVisible={false}
-                        disableNavIfEdgeVisible={false}
-                        disableNavIfEdgeActive={false}
-                        hideNavIfAllVisible={false}
-                        centerMode
-                        updateOnItemClick
-                        itemsListProps={{
-                            style: {
-                                marginRight: '0vw',
-                            },
-                        }}
-                        containerProps={{
-                            style: {
-                                marginTop: '3.5vh',
-                                marginRight: '2vw',
-                                marginLeft: '2vw',
-                                height: '100%',
-                                justifyContent: 'space-between',
-                            },
-                        }}
-                        activeSlideIndex={activeSlide}
-                        activeSlideProps={{
-                            className: 'ActiveSlider',
-                        }}
-                        onRequestChange={setActiveSlide}
-                        forwardBtnProps={{
-                            children: (
-                                <img src={Forward} className="Icons_Arrows" />
-                            ),
-                            style: {
-                                width: '3.5vw',
-                                height: '7vh',
-                                minWidth: 60,
-                                alignSelf: 'center',
-                                border: 'none',
-                                borderRadius: '50%',
-                            },
-                        }}
-                        backwardBtnProps={{
-                            children: (
-                                <img src={Backward} className="Icons_Arrows" />
-                            ),
-                            style: {
-                                width: '3.5vw',
-                                height: '7vh',
-                                minWidth: 60,
-                                alignSelf: 'center',
-                                border: 'none',
-                                borderRadius: '50%',
-                            },
-                        }}
-                        itemsToShow={2}
-                        speed={CheckBrowser()}
-                    >
+                    <div className="Teams_Inner_Row">
                         <div className="Teams_Tiles">
                             <Members.siddharth_anand_srivastav_head />
                         </div>
                         <div className="Teams_Tiles">
                             <Members.raj_aryan_srivastava />
                         </div>
-                        <div className="Teams_Tiles">
+                    </div>
+                    <div className="Teams_Inner_Row1">
+                        <div className="Teams_Tiles1">
                             <Members.pratik_mishra />
                         </div>
-                        <div className="Teams_Tiles">
+                        <div className="Teams_Tiles1">
                             <Members.devi_tanuja_vasamsetty />
                         </div>
-                        <div className="Teams_Tiles">
+                        <div className="Teams_Tiles1">
                             <Members.shivansh_dubey />
                         </div>
-                        <div className="Teams_Tiles">
+                        <div className="Teams_Tiles1">
                             <Members.vislavath_swetha />
                         </div>
-                        <div className="Teams_Tiles">
+                    </div>
+                    <div className="Teams_Inner_Row1">
+                        <div className="Teams_Tiles1">
                             <Members.manoj_gupta />
                         </div>
-                        <div className="Teams_Tiles">
+                        <div className="Teams_Tiles1">
                             <Members.kalpit_adhao />
                         </div>
-                        <div className="Teams_Tiles">
+                        <div className="Teams_Tiles1">
                             <Members.archit_soni />
                         </div>
-                    </Carousel>
+                    </div>
                 </div>
                 <div className="Teams_Mobile_Container">
                     <div className="Teams_Tiles">
@@ -113,25 +53,25 @@ const Designers = () => {
                     <div className="Teams_Tiles">
                         <Members.raj_aryan_srivastava />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.pratik_mishra />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.devi_tanuja_vasamsetty />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.shivansh_dubey />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.vislavath_swetha />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.manoj_gupta />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.kalpit_adhao />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.archit_soni />
                     </div>
                 </div>
--- src/components/Tabs/Teams/External_Affairs.js
+++ src/components/Tabs/Teams/External_Affairs.js
@@ -1,19 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Sub_Files.css'
-import Carousel from 'react-simply-carousel'
 import { Route, Link } from 'react-router-dom'
 import * as Members from './Members_Description_Tiles.js'
-import Forward from './Icons/Front_Arrow.png'
-import Backward from './Icons/Back_Arrow.png'
 
 const External_Affairs = () => {
-    const [activeSlide, setActiveSlide] = useState(0)
-
-    function CheckBrowser() {
-        if (navigator.userAgent.indexOf('Firefox') > -1) return 0
-        return 500
-    }
-
     return (
         <Route>
             <div className="container_box" id="Teams_Scrollbar">
@@ -21,90 +11,43 @@ const External_Affairs = () => {
                     Back
                 </Link>
                 <h1 className="title">EXTERNAL AFFAIRS</h1>
-                <div className="Teams_Carousel_Container">
-                    <Carousel
-                        disableNavIfAllVisible={false}
-                        disableNavIfEdgeVisible={false}
-                        disableNavIfEdgeActive={false}
-                        hideNavIfAllVisible={false}
-                        centerMode
-                        updateOnItemClick
-                        itemsListProps={{
-                            style: {
-                                marginRight: '0vw',
-                            },
-                        }}
-                        containerProps={{
-                            style: {
-                                marginTop: '3.5vh',
-                                marginRight: '2vw',
-                                marginLeft: '2vw',
-                                height: '100%',
-                                justifyContent: 'space-between',
-                            },
-                        }}
-                        activeSlideIndex={activeSlide}
-                        activeSlideProps={{
-                            className: 'ActiveSlider',
-                        }}
-                        onRequestChange={setActiveSlide}
-                        forwardBtnProps={{
-                            children: (
-                                <img src={Forward} className="Icons_Arrows" />
-                            ),
-                            style: {
-                                width: '3.5vw',
-                                height: '7vh',
-                                minWidth: 60,
-                                alignSelf: 'center',
-                                border: 'none',
-                                borderRadius: '50%',
-                            },
-                        }}
-                        backwardBtnProps={{
-                            children: (
-                                <img src={Backward} className="Icons_Arrows" />
-                            ),
-                            style: {
-                                width: '3.5vw',
-                                height: '7vh',
-                                minWidth: 60,
-                                alignSelf: 'center',
-                                border: 'none',
-                                borderRadius: '50%',
-                            },
-                        }}
-                        itemsToShow={3}
-                        speed={CheckBrowser()}
-                    >
-                        <div className="Teams_Tiles">
-                            <Members.aman_kumar />
-                        </div>
+                <div className="Teams_Tile_Container">
+                    <div className="Teams_Inner_Row">
                         <div className="Teams_Tiles">
                             <Members.kushagra_yadav />
                         </div>
                         <div className="Teams_Tiles">
-                            <Members.ankit />
-                        </div>
-                        <div className="Teams_Tiles">
-                            <Members.saurabh_maurya />
-                        </div>
-                        <div className="Teams_Tiles">
-                            <Members.balaka_devivaraprasad />
-                        </div>
-                        <div className="Teams_Tiles">
-                            <Members.vedanth_powar />
-                        </div>
-                        <div className="Teams_Tiles">
-                            <Members.sachin_shaw />
-                        </div>
-                        <div className="Teams_Tiles">
-                            <Members.saumya_chaudhary />
+                            <Members.aman_kumar />
                         </div>
                         <div className="Teams_Tiles">
                             <Members.vikash_prajapati />
                         </div>
-                    </Carousel>
+                    </div>
+                    <div className="Teams_Inner_Row1">
+                        <div className="Teams_Tiles1">
+                            <Members.ankit />
+                        </div>
+                        <div className="Teams_Tiles1">
+                            <Members.saurabh_maurya />
+                        </div>
+                        <div className="Teams_Tiles1">
+                            <Members.balaka_devivaraprasad />
+                        </div>
+                        <div className="Teams_Tiles1">
+                            <Members.vedanth_powar />
+                        </div>
+                    </div>
+                    <div className="Teams_Inner_Row1">
+                        <div className="Teams_Tiles1">
+                            <Members.sachin_shaw />
+                        </div>
+                        <div className="Teams_Tiles1">
+                            <Members.saumya_chaudhary />
+                        </div>
+                        <div className="Teams_Tiles1">
+                            <Members.shashi_kant />
+                        </div>
+                    </div>
                 </div>
                 <div className="Teams_Mobile_Container">
                     <div className="Teams_Tiles">
@@ -116,23 +59,26 @@ const External_Affairs = () => {
                     <div className="Teams_Tiles">
                         <Members.vikash_prajapati />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.ankit />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.saurabh_maurya />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.balaka_devivaraprasad />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.vedanth_powar />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.sachin_shaw />
                     </div>
-                    <div className="Teams_Tiles">
+                    <div className="Teams_Tiles1">
                         <Members.saumya_chaudhary />
+                    </div>
+                    <div className="Teams_Tiles1">
+                        <Members.shashi_kant />
                     </div>
                 </div>
             </div>
